8cf7138c7ba4fef6807c0efecde2264e3b771b4c
Ryoga Wisjnuery <ryogawisjnuery@Ryogas-MacBook-Pro.local>
2023-02-23 11:21:42 +1300

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import sqlalchemy
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('home.html', title="Home")
+
+@app.route("/main")
+def main():
+    return render_template("main.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+@app.errorhandler(404)
+def die(e):
+    return render_template("404.html", title="Error"), 404
+
+if __name__ == "__main__":
+    app.run(debug="true", host="0.0.0.0", port="8000")
